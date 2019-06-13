93d1f52be7213883bb9dfcb45a285dcbd982cc18
= <kazett1@gmail.com>
2019-06-13 17:31:10 +0200

    Fix illegal string offset

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 
 // Original version from: https://stackoverflow.com/questions/6265596/how-to-convert-a-roman-numeral-to-integer-in-php
 
+
 function roman($N)
 {
     $c = 'IVXLCDM';
-    for ($a = 5, $b = $s = ''; $N; $b++, $a ^= 7)
+    for ($a = 5, $b = 0, $s = ''; $N; $b++, $a ^= 7)
     {
         for (
             $o = $N % $a, $N = $N / $a ^ 0;
